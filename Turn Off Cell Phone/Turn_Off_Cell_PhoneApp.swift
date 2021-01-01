ad67b2359216872fb4239486f5c0d99389091e94
Alan Yao <alanyao.training@gmail.com>
2020-12-31 22:45:19 -0800

    Initial Commit

---
+++ Turn Off Cell Phone/Turn_Off_Cell_PhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Turn_Off_Cell_PhoneApp.swift
+//  Turn Off Cell Phone
+//
+//  Created by Alan Yao on 12/31/20.
+//
+
+import SwiftUI
+
+@main
+struct Turn_Off_Cell_PhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
